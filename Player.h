a0d8c84d7bf917f53bd70edd3b7c07ffe54e6d79
Ryan Steed <ryan@steed.org>
2021-12-08 17:33:14 -0700

    Things have been updated. Enemy attack doesn't work for whatever reason I don't understand why.

--- Player.h
+++ Player.h
@@ -5,27 +5,26 @@
 
 #include "commonData.h"
 #include "Enemy.h"
-#include "Item.h"
-#include "Object.h"
 
 using namespace std;
 
 class Player {
 public:
-	Player(string n) : name(n) { lvl = 0; xp = 0; xpthresh = 10; hpmax = 10; hp = hpmax; speed = 10; def = 10; pos = { 15,7 }; }
-	void move(coords&, bool);
+	Player(string n) : name(n) { lvl = 0; xp = 0; xpthresh = 10; hpmax = 10; hp = hpmax; speed = 3; def = 10; atk = 0; pos = { 15,7 }; damage = 2; }
+	void move(coords);
 	void takeDamage(int);
-	void attack(Enemy&);
+	inline void attack(Enemy&);
 	char getChar();
 	coords getPos();
-	int moveCool();
+	double moveCool();
+	double attackCool();
 	void addCooldown(statsCool);
 	void tick();
-	friend ostream& operator << (ostream&, const Player&);
+	bool checkHit();
 
 private:
 	void regen();
-	void gainXP();
+	void gainXP(int);
 	void playerTick(); //Handles all player stat processes that change. Like regen and cooldowns.
 
 	string name;
@@ -37,6 +36,8 @@ private:
 	int hpmax;
 	int speed;
 	int def;
+	int damage;
+	int atk;
 
 	statsCool cooldowns{ 0,0,0 };
 
@@ -50,20 +51,24 @@ coords Player::getPos(){
 	return pos;
 }
 
-void Player::move(coords& c, bool tp) {
-	if (tp) {
-		pos.x = c.x;
-		pos.y = c.y;
-	}
-	else {
-		pos.x += c.x;
-		pos.y += c.y;
-		cooldowns.moveCooldown = 1;
+void Player::move(coords c) {
+	pos.x = c.x;
+	pos.y = c.y;
+	cooldowns.moveCooldown = ((speed - modifiers.sp) * 0.05);
+}
+
+inline void Player::attack(Enemy &e) {
+	if (e.checkHit(rand()%20)) {
+		gainXP((e.takeDamage(rand() % (3) + damage)));
 	}
 }
 
-int Player::moveCool() {
+double Player::moveCool() {
 	return cooldowns.moveCooldown;
+}
+
+double Player::attackCool() {
+	return cooldowns.attackCooldown;
 }
 
 void Player::addCooldown(statsCool s) {
@@ -75,4 +80,13 @@ void Player::tick() {
 	cooldowns.moveCooldown -= (cooldowns.moveCooldown <= 0) ? 0 : 0.05;
 	cooldowns.attackCooldown -= (cooldowns.attackCooldown <= 0) ? 0 : 0.05;
 	cooldowns.regenCooldown -= (cooldowns.regenCooldown <= 0) ? 0 : 0.05;
+}
+
+void Player::gainXP(int i) {
+	xp += 1;
+	if (xp >= xpthresh) {
+		xp -= xpthresh;
+		lvl += 1;
+		xpthresh = xpthresh * 1.5;
+	}
 }
